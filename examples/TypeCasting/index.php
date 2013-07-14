fe76bc1df8f204bd59c268b880204515a64b713c
Serafim <nesk@xakep.ru>
2013-07-14 06:42:06 +0400

    TC example fix

--- examples/TypeCasting/index.php
+++ examples/TypeCasting/index.php
@@ -1,6 +1,3 @@
 <?php
-set_include_path(__DIR__ . '/../');
-define('MIRROR_NO_CACHE', 1);
-#require('lib/Mirror/bootstrap.php');
-require 'phar://../Z:/home/Mirror/mirror.phar';
-echo Mirror::getVersion();
+require('phar://../../mirror.phar');
+require_mirror('test.php');
--- examples/TypeCasting/test.php
+++ examples/TypeCasting/test.php
@@ -1,12 +1,62 @@
 <?php
+class Some
+{
+    public static function subtest(resource $svar, string $sany, object $sololo)
+    {
+        echo 23 . "\n";
+    }
+}
+
+Some::subtest(
+    fopen(__FILE__, 'r'),
+    'asdasd',
+    (object)['asd' => 23]
+);
+
+try {
+    Some::subtest(
+        fopen(__FILE__, 'r'),
+        'asdasd',
+        ['asd' => 23] // NOT OBJECT == Exception
+    );
+} catch (\Exception $e) {
+    echo 'Exception: ' . $e->getMessage() . "\n";
+}
+
+
+/*
+ * ANOTHER TESTS
+ */
+
 function test(int $tsome, $tany = 42)
 {
-    echo $tsome;
+    echo $tsome . ' : ' . $tany . "\n";
 }
-
 test(42);
+test(23, 42);
 
-function subtest(int $svar, str $sany, object $sololo)
-{
-
+try {
+    test('error string'); // Exception
+} catch (\Mirror\Exceptions\TypeException $e) {
+    echo 'Exception: ' . $e->getMessage(). "\n\n";
 }
+
+
+/*
+ * AVAILABLE CASTS
+ */
+echo 'CASTS: ' . implode(', ', \Mirror\Glass\Std\TypeCasting::getTypeCasts());
+
+
+
+
+
+/*** Result: ***
+23
+Exception: Value `Array([asd] => 23)` is not object
+42 : 42
+23 : 42
+Exception: Value `error string` is not integer
+
+CASTS: int, integer, str, string, object, res, resource, array, double, float, scalar, callable, func, bool, boolean
+ */
